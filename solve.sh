26172cee6d9bf06fd73412b26160f8553a25b543
Shae Brown <shaeb@uvic.ca>
2017-03-30 18:29:53 -0700

    Added script to generate a report and batch run tests

---
+++ solve.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Test files named 01.txt, 02.txt .. num_files.txt
+#Produces a report in sol.txt
+num_files=95
+time_sum=0
+TIME=0
+mkdir cnf
+mkdir cnfsol
+for i in $(seq -f "%02g" 1 ${num_files})
+do
+   input="./hard/${i}.txt"
+   out="./cnf/cnf${i}.txt"
+   python sud2sat.py -i ${input} > ${out}
+done
+for i in $(seq -f "%02g" 1 ${num_files})
+do
+   input="./cnf/cnf${i}.txt"
+   out="./cnfsol/cnfsol${i}.txt"
+   TIME=$(minisat -verb=2 ${input} ${out} | grep -o [0-9+]\.[0-9+]* | tail -1 | bc -l)
+   time_sum=`echo "$time_sum + $TIME" | bc`
+   printf "${i}\n" >> sol.txt
+   printf "Time: ${TIME}s\n" >> sol.txt
+   python sat2sud.py -i ${out} >> sol.txt
+   printf "\n\n" >> sol.txt
+done
+echo "TOTAL TIME: ${time_sum}s" >> sol.txt
+rm -r cnf
+rm -r cnfsol
